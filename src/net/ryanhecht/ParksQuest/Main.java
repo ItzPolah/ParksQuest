f4595de1d30dd317a7948d49f3363b4f14037084
RyanHecht <ryanhecht.tse@gmail.com>
2015-11-20 07:36:59 -0500

    added enabled/disabled messages

--- src/net/ryanhecht/ParksQuest/Main.java
+++ src/net/ryanhecht/ParksQuest/Main.java
@@ -1,5 +1,12 @@
 package net.ryanhecht.ParksQuest;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+	public void onEnable() {
+	getLogger().info("ParksQuest has been enabled.");	
+	}
+	public void onDisable() {
+	getLogger().info("ParksQuest has been disabled.");
+	}
 }
