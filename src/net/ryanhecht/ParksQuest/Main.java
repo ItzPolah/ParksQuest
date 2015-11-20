36a989b729958b2165b9d27a3a9f74f38d41d751
Ryan Hecht <ryanhecht.tse@gmail.com>
2015-11-20 01:22:36 -0500

    Initial testing of NPC ArmorStands

--- src/net/ryanhecht/ParksQuest/Main.java
+++ src/net/ryanhecht/ParksQuest/Main.java
@@ -1,5 +1,10 @@
 package net.ryanhecht.ParksQuest;
 
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,5 +13,17 @@ public class Main extends JavaPlugin {
 	}
 	public void onDisable() {
 		
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	
+		Player player = (Player) sender;
+		if(cmd.getName().equalsIgnoreCase("pq")) {
+			NPC npc = new NPC(player.getLocation());
+			npc.walk(new Location(player.getWorld(), player.getLocation().getX()+5, player.getLocation().getY(), player.getLocation().getZ()));
+		}
+		return false;
+	}
+	public static Plugin getPlugin() {
+		return Main.getPlugin(Main.class);
 	}
 }
