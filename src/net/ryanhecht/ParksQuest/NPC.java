3f138fe83c214eb24ca0a15db6a95c8e580018e0
Ryan Hecht <ryanhecht.tse@gmail.com>
2015-11-22 00:29:17 -0500

    Arm/leg movements in NPC#walk() worked on, pretty happy with them now

--- src/net/ryanhecht/ParksQuest/NPC.java
+++ src/net/ryanhecht/ParksQuest/NPC.java
@@ -43,17 +43,17 @@ public class NPC {
 	}
 	private void walkAnimate() {
 		if(i.equals(1)) {
-           	stand.setLeftArmPose(new EulerAngle(stand.getLeftArmPose().getX()-.5, stand.getLeftArmPose().getY(), stand.getLeftArmPose().getZ()));
-        	stand.setRightArmPose(new EulerAngle(stand.getRightArmPose().getX()+.5, stand.getRightArmPose().getY(), stand.getRightArmPose().getZ()));
-        	stand.setLeftLegPose(new EulerAngle(stand.getLeftLegPose().getX()+.5, stand.getLeftLegPose().getY(), stand.getLeftLegPose().getZ()));
-        	stand.setRightLegPose(new EulerAngle(stand.getRightLegPose().getX()-.5, stand.getRightLegPose().getY(), stand.getRightLegPose().getZ()));
+           	stand.setLeftArmPose(new EulerAngle(stand.getLeftArmPose().getX()-(Math.PI/20), stand.getLeftArmPose().getY(), stand.getLeftArmPose().getZ()));
+        	stand.setRightArmPose(new EulerAngle(stand.getRightArmPose().getX()+(Math.PI/20), stand.getRightArmPose().getY(), stand.getRightArmPose().getZ()));
+        	stand.setLeftLegPose(new EulerAngle(stand.getLeftLegPose().getX()+(Math.PI/20), stand.getLeftLegPose().getY(), stand.getLeftLegPose().getZ()));
+        	stand.setRightLegPose(new EulerAngle(stand.getRightLegPose().getX()-(Math.PI/20), stand.getRightLegPose().getY(), stand.getRightLegPose().getZ()));
 
 		}
 		if(i.equals(-1)) {
-           	stand.setLeftArmPose(new EulerAngle(stand.getLeftArmPose().getX()+.5, stand.getLeftArmPose().getY(), stand.getLeftArmPose().getZ()));
-        	stand.setRightArmPose(new EulerAngle(stand.getRightArmPose().getX()-.5, stand.getRightArmPose().getY(), stand.getRightArmPose().getZ()));
-        	stand.setLeftLegPose(new EulerAngle(stand.getLeftLegPose().getX()-.5, stand.getLeftLegPose().getY(), stand.getLeftLegPose().getZ()));
-        	stand.setRightLegPose(new EulerAngle(stand.getRightLegPose().getX()+.5, stand.getRightLegPose().getY(), stand.getRightLegPose().getZ()));
+           	stand.setLeftArmPose(new EulerAngle(stand.getLeftArmPose().getX()+(Math.PI/20), stand.getLeftArmPose().getY(), stand.getLeftArmPose().getZ()));
+        	stand.setRightArmPose(new EulerAngle(stand.getRightArmPose().getX()-(Math.PI/20), stand.getRightArmPose().getY(), stand.getRightArmPose().getZ()));
+        	stand.setLeftLegPose(new EulerAngle(stand.getLeftLegPose().getX()-(Math.PI/20), stand.getLeftLegPose().getY(), stand.getLeftLegPose().getZ()));
+        	stand.setRightLegPose(new EulerAngle(stand.getRightLegPose().getX()+(Math.PI/20), stand.getRightLegPose().getY(), stand.getRightLegPose().getZ()));
 		}
 		if(i.equals(0)) {
         	stand.setLeftArmPose(new EulerAngle(0,0,0));
@@ -61,11 +61,11 @@ public class NPC {
         	stand.setRightLegPose(new EulerAngle(0,0,0));
         	stand.setLeftLegPose(new EulerAngle(0,0,0));	
 		}
-		if(Math.abs(stand.getLeftArmPose().getX() - (Math.PI/4)) <= 1) {
-			i = new Integer(-1);
-		}
-		if(Math.abs(stand.getLeftArmPose().getX() + (Math.PI/4)) <= 1) {
+		if(Math.abs(stand.getLeftArmPose().getX() - (Math.PI/4)) <= .1) {
 			i = new Integer(1);
+		}
+		if(Math.abs(stand.getLeftArmPose().getX() + (Math.PI/4)) <= .1) {
+			i = new Integer(-1);
 		}
 		Bukkit.getServer().getLogger().info((Math.abs(stand.getLeftArmPose().getX())+""));
 	}
